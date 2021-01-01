94631bc877315cb2b4424cc1e0d34bb1f4861026
Jeanette Perez <jeanette.marie.perez@gmail.com>
2020-12-31 19:55:56 -0600

    adding js file

---
+++ js/interactive.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let firstMessage = "Hi there! Whovian";
+alert(firstMessage);
+let favoriteDoctor = prompt("Which doctor is your favorite doctor?");
+let response = favoriteDoctor
+alert("That's Awesome, " + response + " is absolutely brilliant!" )
