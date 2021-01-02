f8979ae3232fb61efd8fe7aca54dc047a5de6156
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-01-02 17:54:40 -0600

    learning about search boxes

--- js/interactive.js
+++ js/interactive.js
@@ -1,7 +1,12 @@
 "use strict"
+/*
+function welcomeMessage() {
+    let firstMessage = "Hi there! Whovian";
+    alert(firstMessage);
+    let favoriteDoctor = prompt("Which doctor is your favorite doctor?");
+    let response = favoriteDoctor
+    alert("That's Awesome, " + response + " is absolutely brilliant!" )
+} welcomeMessage();
 
-let firstMessage = "Hi there! Whovian";
-alert(firstMessage);
-let favoriteDoctor = prompt("Which doctor is your favorite doctor?");
-let response = favoriteDoctor
-alert("That's Awesome, " + response + " is absolutely brilliant!" )
+
+ */
